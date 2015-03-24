34a9548886e847d6d1de6cc15e31a249169edcc7
Simone Soldateschi <simone.soldateschi@gmail.com>
2015-03-24 22:16:33 +1100

    Add Integration Tests

--- recipes/nodejs.rb
+++ recipes/nodejs.rb
@@ -49,4 +49,4 @@ end
 
 # Open http://localhost:80 to view your presentation
 
-add_iptables_rule('INPUT', '-p tcp -m state --state NEW --dport 80 -s 10.0.0.0/8 -j ACCEPT', 50, 'Allow the Internet access the web-server')
+add_iptables_rule('INPUT', '-p tcp -m state --state NEW --dport 80 -s 0.0.0.0/0 -j ACCEPT', 50, 'Allow the Internet access the web-server')
